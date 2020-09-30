00ecf68c38c3d6737812e9a30afed6bba2a19a22
Tobias <tobi5457@edu.easj.dk>
2020-09-30 09:08:07 +0200

    Styles burgermenu ikon med div'er

--- header.php
+++ header.php
@@ -13,9 +13,9 @@
         <img class="topLogo" src="img/Toplogo.png" alt="Toplogo">
     </div>
 
-    <div class="threeIcons d-flex">
-        <div class="mr-2"><i class="fas fa-heart"></i></div>
-        <div class="mx-2"><i class="fas fa-shopping-cart"></i></div>
+    <div class="threeIcons d-flex align-items-center">
+        <div class="mr-2 pb-1"><i class="fas fa-heart"></i></div>
+        <div class="mx-2 pb-1"><i class="fas fa-shopping-cart"></i></div>
         <div class="burgerBarContainer py-3 pl-2 d-md-none clicked">
             <div class="burgerBar"></div>
         </div>
