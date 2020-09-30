9f27afb18e6d7ba5b0a1186fafcc1174b8ca9d85
Mostafa <Mostafa_haidare@yahoo.com>
2020-09-30 08:18:11 +0200

    start på header.php

--- header.php
+++ header.php
@@ -9,11 +9,16 @@
 <body>
 
 
-<header class="container-fluid px-4">
-    <div class="Header-section1">
-        <img src="img/Toplogo.png" alt="Toplogo">
-
+<header class="container-fluid px-4 d-flex justify-content-between align-items-center">
+    <div class="Header-section1 d-flex">
+        <img class="topLogo" src="img/Toplogo.png" alt="Toplogo">
     </div>
+
+    <div class="threIcons d-flex">
+        <div class="pr-2"><p>ssss</p></div>
+        <div class="px-2"><p>sssss</p></div>
+        <div class="pl-2"><p>sssss</p></div>
+     </div>
 </header>
 
 
