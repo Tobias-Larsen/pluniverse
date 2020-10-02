c3b096d354af9f985c0b430d77285c065cffda44
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-02 12:19:07 +0200

    swiper blev fixed

--- forsideSwiper.php
+++ forsideSwiper.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-
 <!-- Swiper -->
 <div class="swiper-container">
     <div class="swiper-wrapper">
-        <div class="slide1 swiper-slide"></div>
-        <div class="swiper-slide"><img src="img/b1.jpg"></div>
+        <div data-background="img/b6.png" class="swiper-slide swiper-lazy slide1">
+            <div class="swiper-lazy-preloader"></div>
+        </div>
+        <!--<div class="swiper-slide"><img src="img/b1.jpg"></div>-->
         <div class="swiper-slide"><img src="img/b2.jpg"></div>
         <div class="swiper-slide"><img src="img/b3.jpg"></div>
         <div class="swiper-slide"><img src="img/b4.png"></div>
@@ -24,24 +22,4 @@
 </div>
 
 
-<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script>
-    $(function () {
-
-        var swiper = new Swiper('.swiper-container', {
-            slidesPerView: 1,
-            spaceBetween: 30,
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        })
-    });
-</script>
 </html>
