bde69dc2efc65dc345ca560450a32dbbff487afa
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-05 14:37:07 +0200

    oprettet produkter.php og importProdukter.php til projektet

---
+++ importProdukter.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link href="css/stylesHeader.min.css" rel="stylesheet" type="text/css">
+<link href="css/stylesFooter.min.css" rel="stylesheet" type="text/css">
+<link href="css/produkter.min.css" rel="stylesheet" type="text/css">
