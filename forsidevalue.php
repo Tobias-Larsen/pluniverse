541d7d5fdaef01169e273d69c6efb00da7c6809b
piga-commits <git@piga.one>
2020-10-05 14:26:18 +0200

    har flyttet menuscript ind i burgermenu.php

--- forsidevalue.php
+++ forsidevalue.php
@@ -1,5 +1,68 @@
 
 
-<div class="container-fluid pt-5">
-    <div class=""></div>
+<div class="frontValues container-fluid pt-5 alignright danskDesign">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+<p>Lorem ipsum dolor sit amet, consectetur
+    adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
+</div>
+<div class="frontValues container-fluid pt-5 alignleft certifikater">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
+</div>
+<div class="frontValues container-fluid pt-5">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
+</div>
+<div class="frontValues container-fluid pt-5">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
+</div>
+<div class="frontValues container-fluid pt-5">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
+</div>
+<div class="frontValues container-fluid pt-5">
+    <img src="img/testBillede.jpg">
+    <div class="valueTitel"></div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.</p>
+
+    <a href="#" class="valueLink">Se mere her >>></a>
+
 </div>
