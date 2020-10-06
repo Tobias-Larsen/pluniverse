959769d539359a5e476bcbb04651a7fbbf653e47
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-06 14:12:52 +0200

    Erhverv produkter side er blevet færdig.

--- alleProdukter.php
+++ alleProdukter.php
@@ -1,6 +1,47 @@
+<div class="alleProdukter flex-nowrap flex-column flex-md-row container-fluid d-flex flex-md-wrap">
+    <p class="Pnavn container-fluid d-flex justify-content-center mt-4 mb-2">Produkter</p>
+    <p class="textInfo container-fluid mb-4 px-3 d-flex justify-content-center">Alle produkter kan fås, med eget logo på Labels eller Swingtags.</p>
 
-<div class="alleProdukter bg-primary container-fluid">
-    <p class="Pnavn">Produkter</p>
-    <div>
+     <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Haj</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+        <div class="produktbillede1"></div>
+        <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
     </div>
+
+    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Hval</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+    <div class="produktbillede1"></div>
+    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    </div>
+
+    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Dinosaur</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+    <div class="produktbillede1"></div>
+    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    </div>
+
+    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Krokodille</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+    <div class="produktbillede1"></div>
+    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    </div>
+
+    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Pingvin</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+    <div class="produktbillede1"></div>
+    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    </div>
+
+    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+        <div class="Hej ml-3 mt-3 position-absolute"><p>Elefant</p></div>
+        <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
+    <div class="produktbillede1"></div>
+    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    </div>
+
 </div>
--- importProdukter.php
+++ importProdukter.php
@@ -1,8 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
 <link href="css/stylesHeader.min.css" rel="stylesheet" type="text/css">
 <link href="css/stylesFooter.min.css" rel="stylesheet" type="text/css">
 <link href="css/stylesBurger.min.css" rel="stylesheet" type="text/css">
-
-<link href="css/produkter.min.css" rel="stylesheet" type="text/css">
-<link href="css/alleprodukter.min.css" rel="stylesheet" type="text/css">
+ <link href="css/alleprodukter.min.css" rel="stylesheet" type="text/css">
 
