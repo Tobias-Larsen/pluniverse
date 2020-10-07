e821a2852a77e6e7c4ccef0994ef66786288bdb2
Daniel "Hulapyk" Jørgensen <danielsj95@hotmail.com>
2020-10-07 12:45:06 +0200

    Lavet produktunderside og dertilhørende filer

--- imports.php
+++ imports.php
@@ -9,3 +9,4 @@
 <link href="css/stylesBurger.min.css" rel="stylesheet" type="text/css">
 <link href="css/stylesFooter.min.css" rel="stylesheet" type="text/css">
 <link href="css/StylesFAQ.min.css" rel="stylesheet" type="text/css">
+<link href="css/stylesProduktUnderside.min.css" rel="stylesheet" type="text/css">
---
+++ produktunderside.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <!-- <link href="css/stylesProduktUnderside.min.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    </head>
+    
+
+<body>
+
+<div class="produktUContainer">
+
+<h2>Haj</h2>
+
+<div class="PUImgContainer">
+
+    <div class="PULargeImg">
+        <img src="img/shark1.png" alt="shark">
+    </div>
+
+    <div class="PUSmallImg">
+        <img src="img/shark2.png" alt="shark">
+    </div>
+
+    <div class="PUSmallImg">
+        <img src="img/shark3.png" alt="shark">
+    </div>
+
+    <div class="PUSmallImg">
+        <img src="img/shark4.png" alt="shark">
+    </div>
+
+    <div class="PUSmallImg parentOverlay">
+        <img src="img/shark5.png" alt="shark">        
+        <div class="gradientOverlay"></div>
+        <div class="arrowOverlay">
+            <img src="img/moreImgArrow.png" alt="arrow">
+        </div>
+        <div class="minioverlay"></div>
+    </div>
+
+</div>
+
+
+
+<div class="prisContainer">
+
+    <p class="prisText">Fra <span class="prisStor">149 DKK</span> pr. stk. </p>
+
+    <div class="prisKnap">
+        <div class="prisKnapWrap">
+            <p>Tilføj til beregner</p>
+            <img src="img/beregnerImg.png" alt="calc">
+        </div>
+    </div>
+</div>
+
+
+
+<div class="produktText">
+
+    <p>Ring eller skriv til os, for et skræddersyet tilbud!<br>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur <br>
+        adipiscing elit, sed do eiusmod tempor <br>
+        incididunt ut labore et dolore magna aliqua.
+    </p>
+
+    <p class="hiddenMobile">
+        Vægt: 0.45 kg<br>
+        Længde: 38 cm<br>
+        Bredde: 21 cm
+    </p>
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+<script>
+
+</script>
+
+</body>
+</html>
