414be9ecafbcb73543696f0a8c4e0e84aff1458d
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-08 11:53:26 +0200

    fade ind til alleProdukter.php siden blev tilføjet

--- alleProdukter.php
+++ alleProdukter.php
@@ -1,4 +1,4 @@
-<div class="alleProdukter pt-4 flex-nowrap flex-column flex-md-row container-fluid d-flex flex-md-wrap">
+<div  class="alleProdukter pt-4 flex-nowrap flex-column flex-md-row container-fluid d-flex flex-md-wrap">
     <p class="Pnavn container-fluid d-flex justify-content-center mt-4 mb-2">Produkter</p>
     <p class="textInfo container-fluid mb-4 px-3 d-flex justify-content-center">Alle produkter kan fås, med eget logo på Labels eller Swingtags.</p>
 
@@ -9,7 +9,7 @@
     <a href="enkeltProdukt.php" <button type="button"class="produktBTN text-light d-flex align-items-center justify-content-center btn position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-right" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Hval</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
     <div class="produktbillede2"></div>
@@ -23,21 +23,21 @@
 <a href="enkeltProdukt.php"<button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div data-aos="fade-left" class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-right" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Krokodille</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
     <div class="produktbillede4"></div>
 <a href="enkeltProdukt.php" <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-right" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Pingvin</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
     <div class="produktbillede5"></div>
 <a href="enkeltProdukt.php"<button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-right"class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Elefant</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
     <div class="produktbillede6"></div>
