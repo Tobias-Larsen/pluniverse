29fa4e07fd6be01bda5862077017ffd9492bf5b7
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-08 11:35:15 +0200

    header blev fixed til tablet og PC

--- alleProdukter.php
+++ alleProdukter.php
@@ -2,46 +2,46 @@
     <p class="Pnavn container-fluid d-flex justify-content-center mt-4 mb-2">Produkter</p>
     <p class="textInfo container-fluid mb-4 px-3 d-flex justify-content-center">Alle produkter kan fås, med eget logo på Labels eller Swingtags.</p>
 
-     <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+     <div data-aos="fade-right" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Haj</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
         <div class="produktbillede1"></div>
-        <button type="button"class="produktBTN text-light d-flex align-items-center justify-content-center btn position-absolute">SE PRODUKT</button>
+    <a href="enkeltProdukt.php" <button type="button"class="produktBTN text-light d-flex align-items-center justify-content-center btn position-absolute">SE PRODUKT</button></a>
     </div>
 
     <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Hval</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
-    <div class="produktbillede1"></div>
-    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    <div class="produktbillede2"></div>
+<a href="enkeltProdukt.php" <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-right" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Dinosaur</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
-    <div class="produktbillede1"></div>
-    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    <div class="produktbillede3"></div>
+<a href="enkeltProdukt.php"<button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
-    <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
+    <div data-aos="fade-left" class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Krokodille</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
-    <div class="produktbillede1"></div>
-    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    <div class="produktbillede4"></div>
+<a href="enkeltProdukt.php" <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
     <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Pingvin</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
-    <div class="produktbillede1"></div>
-    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    <div class="produktbillede5"></div>
+<a href="enkeltProdukt.php"<button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
     <div class="mb-4 col-sm-12 col-md-6 col-lg-4">
         <div class="Hej ml-3 mt-3 position-absolute"><p>Elefant</p></div>
         <div class="prise1 ml-3 mt-5 position-absolute"<p>Fra 149 DKK pr. stk.</p></div>
-    <div class="produktbillede1"></div>
-    <button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button>
+    <div class="produktbillede6"></div>
+    <a href="enkeltProdukt.php"<button type="button"class="produktBTN d-flex align-items-center justify-content-center btn btn-primary position-absolute">SE PRODUKT</button></a>
     </div>
 
 </div>
