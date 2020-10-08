f62ec8acae4a9954be852ce4af3f2597be1cfc4e
Mostafa <Mostafa_haidare@yahoo.com>
2020-10-08 14:36:20 +0200

    produkter side blev færdig

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
         </div>
 
 
-        <div class="nav-Desktop w-100 d-none d-md-flex flex-row justify-content-md-center">
+        <div class="nav-Desktop balooFont w-100 d-none d-md-flex flex-row justify-content-md-center">
             <div class="px-4"><a href="produkter.php">Produkter</a></div>
             <div class="px-4"><a href="designdinegen.php#">Design din egen</a></div>
             <div class="px-4"><a href="omos.php#">Om os</a></div>
